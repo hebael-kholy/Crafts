5855ac376a468083d94e13b739aff2593799f962
heba12377 <hebaelsaidali@gmail.com>
2023-05-18 13:39:05 +0200

    Eidt and Delete Coupon

--- Crafts.Api/Controllers/CouponsController.cs
+++ Crafts.Api/Controllers/CouponsController.cs
@@ -1,4 +1,6 @@
-﻿using Crafts.BL.Dtos.CouponDtos;
+﻿using Crafts.BL.Dtos;
+using Crafts.BL.Dtos.CouponDtos;
+using Crafts.BL.Dtos.ReviewDtos;
 using Crafts.BL.Managers.CouponManager;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,13 +21,26 @@ namespace Crafts.Api.Controllers
         [HttpPost]
         public async Task<ActionResult> Add(CouponAddDto couponDto)
         {
-            await _couponsManagers.Add(couponDto);
-            return Ok(couponDto);
+            try
+            {
+                await _couponsManagers.Add(couponDto);
+                var msg = new GeneralResponse("Coupon Added Successfully");
+                var res = new { msg, couponDto };
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet]
         public ActionResult<List<CouponReadDto>> GetAll()
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             return _couponsManagers.GetAll();
         }
 
@@ -34,7 +49,38 @@ namespace Crafts.Api.Controllers
         public ActionResult<CouponReadDto> GetById(int id)
         {
             var coupon = _couponsManagers.GetById(id);
+            if (coupon == null)
+            {
+                return NotFound(new GeneralResponse("Resource is missing"));
+            }
             return coupon;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            _couponsManagers.Delete(id);
+            var msg = new GeneralResponse($"Coupon with id {id} Deleted Successfully");
+            var res = new { msg, id };
+            return Ok(res);
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public ActionResult Edit(CouponEditDto coupon, int id)
+        {
+            try
+            {
+                _couponsManagers.Edit(coupon, id);
+                var msg = new GeneralResponse($"Coupon with id {id} Updated Successfully");
+                var res = new { msg, id, coupon};
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
